64ab5956f861b554c6d49f24ada1b0f2530d3cda
Yakov <16872793+yakov116@users.noreply.github.com>
2022-07-11 08:58:29 -0400

    draft

---
+++ source/features/pr-list-avatar.tsx
@@ -0,0 +1,35 @@
+
+import React from 'dom-chef';
+import select from 'select-dom';
+import * as pageDetect from 'github-url-detection';
+
+import features from '.';
+import getUserAvatar from '../github-helpers/get-user-avatar';
+
+async function highlightCollaborators(): Promise<void> {
+	for (const author of select.all('.js-issue-row [data-hovercard-type="user"]')) {
+		const src = getUserAvatar(author.textContent!.trim(), 16)!;
+
+		const avatar = (
+			<img
+				className="avatar mr-2"
+				src={src}
+				width="16"
+				height="16"
+				alt={`@${author.textContent!.trim()}`}
+			/>
+		);
+		author.prepend(avatar);
+	}
+}
+
+void features.add(import.meta.url, {
+	include: [
+		pageDetect.isRepoConversationList,
+	],
+	exclude: [
+		() => select.exists('.blankslate'),
+	],
+	deduplicate: 'has-rgh-inner',
+	init: highlightCollaborators,
+});
